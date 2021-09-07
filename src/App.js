79f29f2997e87cc946fdac5dd15949fb072072ac
Alexander Hansen <Alexander-hansen@hotmail.com>
2021-09-08 00:32:44 +0200

    s

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/styles.scss';
 
 import Heading from './compnents/Heading'
+import MeetOlarm from './compnents/MeetOlarm';
 
 function App() {
   return <div className="App">
     <Heading/>
-    
+    <MeetOlarm/>
   </div>
   
 }
---
+++ src/compnents/MeetOlarm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+function MeetOlarm() {
+    return <Container fluid className='meetOlarm'>
+     
+  </Container>
+}
+
+export default MeetOlarm
