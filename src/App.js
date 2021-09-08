ba7bf6259b3e21d6ad0877dbaeefd4710f150a05
Alexander Hansen <Alexander-hansen@hotmail.com>
2021-09-08 11:12:46 +0200

    s

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/styles.scss';
 
 import Heading from './compnents/Heading'
-import MeetOlarm from './compnents/MeetOlarm';
 
 function App() {
   return <div className="App">
     <Heading/>
-    <MeetOlarm/>
   </div>
   
 }
--- src/compnents/Heading.js
+++ src/compnents/Heading.js
@@ -1,56 +1,65 @@
-import React from 'react'
+import React from "react";
 
-import Container from 'react-bootstrap/Container'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
-
-import Logo from '../img/alarm-logo.png'
-
-
-
+import Logo from "../img/alarm-logo.png";
 
 function Heading() {
-    return <Container fluid className='heading'>
-              <Navbar collapseOnSelect expand="lg" bg="light" variant="dark">
-  <Container>
-  <Navbar.Brand href="#home">
-    <img src={Logo} className='logo' />
-  </Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto" fill>
-      <Nav.Link href="#features">Home</Nav.Link>
-      <Nav.Link href="#pricing">Features</Nav.Link>
-      <Nav.Link href="#pricing">Plans</Nav.Link>
-      <Nav.Link href="#pricing">Meet Olarm</Nav.Link>
-    </Nav>
-    <Nav>
-      <Nav.Link href="#deets">Sign up</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Sign in
-      </Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
-<Container className='headers'>
-  <Row>
-    <Col>
-    <h1>
-      World's <span>Smartest</span> Home Security
-    </h1>
-    <p>Unique security experiences with more than 300,000 clients, Simple Installation, Convenience & Free Monitoring</p>
-    <Button variant="primary">Primary</Button>{' '}
-    </Col>
-    <Col></Col>
-  </Row>
-</Container>
-          </Container>
- 
+  return (
+    <Container fluid className="heading">
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={Logo} className="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto" fill>
+              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#pricing">Features</Nav.Link>
+              <Nav.Link href="#pricing">Plans</Nav.Link>
+              <Nav.Link href="#pricing">Meet Olarm</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link>Sign up</Nav.Link>
+              <Nav.Link className="active signIn">Sign in</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container className="headers">
+        <Row>
+          <Col>
+            <h1>
+              World's <span>Smartest</span> Home Security
+            </h1>
+            <p>
+              Unique security experiences with more than 300,000 clients, Simple
+              Installation, Convenience & Free Monitoring
+            </p>
+            <Button variant="primary">Visit shop</Button>{" "}
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+      <Container fluid>
+        <Row>
+          <Col>
+            <div className="some-icons">
+              <i class="fab fa-google"></i>
+              <i class="fab fa-twitter"></i>
+              <i class="fab fa-facebook-f"></i>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
 }
 
-export default Heading
+export default Heading;
